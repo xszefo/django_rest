3c3ab1679d0f4eed1904d0c8609c287c3802c328
Piotr Sumelka <piotr.sumelka@gmail.com>
2021-05-15 15:01:34 +0200

    api_post

--- myrestapi/api/views.py
+++ myrestapi/api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 import todo.models as todo
 from .serializers import TaskSerializer
@@ -10,3 +11,11 @@ class TaskListAPI(APIView):
         objects = todo.Task.objects.all()
         serializer = TaskSerializer(objects, many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = TaskSerializer(data=request.data)
+		
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
