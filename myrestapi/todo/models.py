79c546cf0c1334c1c0ab209e133e9a57bf377da3
Piotr Sumelka <piotr.sumelka@gmail.com>
2021-05-15 14:29:53 +0200

    listview

--- myrestapi/todo/models.py
+++ myrestapi/todo/models.py
@@ -8,3 +8,6 @@ class Task(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
     deadline = models.DateTimeField(auto_now=False, auto_now_add=False, null=True, blank=False)
     created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- myrestapi/todo/views.py
+++ myrestapi/todo/views.py
@@ -2,7 +2,5 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Task
 
-# Create your views here.
-
 class TodoList(ListView):
     queryset = Task.objects.all()
