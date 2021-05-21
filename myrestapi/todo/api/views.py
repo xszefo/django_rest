1ab1161e5cbfad25279e6f65bce0262120854dfa
Piotr Sumelka <piotr.sumelka@gmail.com>
2021-05-21 20:30:51 +0200

    pagination

--- myrestapi/todo/api/views.py
+++ myrestapi/todo/api/views.py
@@ -2,26 +2,34 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import generics
 
 import todo.models as TODO
 from .serializers import TaskSerializer, TaskDetailSerializer
+from .pagination import TaskPaginationPage
 
-class TaskListAPI(APIView):
-    def get(self, request):
-        context = {
-            'request': request,
-        }
-        objects = TODO.Task.objects.all()
-        serializer = TaskSerializer(objects, many=True, context=context)
-        return Response(serializer.data)
+class TaskListAPI(generics.ListAPIView):
+    queryset = TODO.Task.objects.all()
+    pagination_class = TaskPaginationPage
+    serializer_class = TaskSerializer
+    
+# class TaskListAPI(APIView):
+#     def get(self, request):
+#         context = {
+#             'request': request,
+#         }
+#         pagination_class = TaskPaginationPage
+#         queryset = TODO.Task.objects.all()
+#         serializer = TaskSerializer(queryset, many=True, context=context)
+#         return Response(serializer.data)
 
-    def post(self, request):
-        serializer = TaskSerializer(data=request.data)
+#     def post(self, request):
+#         serializer = TaskSerializer(data=request.data)
 		
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class TaskDetailsAPI(APIView):
     def get_object(self, pk):
