aa17d54ff760a7dbd3e81a93becfcc9ee3e6951d
Piotr Sumelka <piotr.sumelka@gmail.com>
2021-05-25 17:30:19 +0200

    jwt-authorization

--- myrestapi/myrestapi/urls.py
+++ myrestapi/myrestapi/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,5 +23,5 @@ urlpatterns = [
     path('todo/', include('todo.urls')),
     path('api/todo/', include('todo.api.urls', namespace='api-todo')),
     path('api/comments/', include('comments.api.urls', namespace='comments-todo')),
-
+    path('api/auth/token', obtain_jwt_token)
 ]
--- myrestapi/todo/api/views.py
+++ myrestapi/todo/api/views.py
@@ -1,8 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import NotFound
 
 import todo.models as TODO
@@ -15,8 +13,6 @@ from .pagination import TaskPaginationPage
 #     serializer_class = TaskSerializer
     
 class TaskListAPI(APIView):
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
     def get(self, request):
         context = {
             'request': request,
